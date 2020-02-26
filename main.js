11ebfe543fba9fa7c1addfbed52052d4642cc3b6
scastine <scastine@f01898389bde.ant.amazon.com>
2020-02-26 20:11:50 -0300

    removed jquery and fixed cors issue

--- main.js
+++ main.js
@@ -4,19 +4,10 @@ buildHeatMap();
 
 url = 'https://6ine6u27z5.execute-api.us-east-1.amazonaws.com/default/getStatsForMoment';
 
-$(document).ready(function() {
-    $.ajax({
-      url: url,
-      crossDomain: true,
-      beforeSend: function(xhr) {
-        xhr.withCredentials = true;
-      },
-      success: function(response) {
-        var resp = JSON.parse(response)
-        alert(resp.status);
-      },
-      error: function(xhr, status) {
-        alert("error");
-      }
-    });
-});
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
