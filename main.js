d411aef311efc6c8c1f92ccf7436d38bedf0eab7
scastine <scastine@f01898389bde.ant.amazon.com>
2020-03-02 14:55:55 -0300

    fixed b ugs

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 url = 'https://6ine6u27z5.execute-api.us-east-1.amazonaws.com/default/getStatsForMoment';
 
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    buildCount(data);
-    buildHeatMap(data);
-  });
+loadData();
+
+function loadData(){
+  var queryParam = '?substract=' + document.getElementById("period").value;
+  fetch(url + queryParam)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      buildCount(data);
+      buildHeatMap(data);
+    });
+}
